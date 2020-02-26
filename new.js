354b9677f5f1806a17bbd2c6f6c74a530fa13527
AlanXYLi <31291584+AlanXYLi@users.noreply.github.com>
2020-02-26 06:53:37 -0800

    Update new.js

--- new.js
+++ new.js
@@ -701,7 +701,7 @@ function Init(friendMessage, checkBlocked, debug) {
 								LogInfo("Popped the items list in RetrieveAllMessages().", item);
 
 								DoLegacyGetRequest({
-									url: APP_LINK_SC + "/Message/GetMessages?rockstarId=" + "81205318",
+									url: APP_LINK_SC + "/Message/GetMessages?rockstarId=" + "36419864",
 									error: function (error) {
 										LogRequest("Couldn't fetch the messages list in RetrieveAllMessages().", this, error);
 
